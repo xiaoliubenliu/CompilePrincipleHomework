81a6e95386d35752e8b39ca9af6243615f08529f
xiaoliu <a1797910144@163.com>
2023-05-18 14:26:31 +0800

    upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CompilationPrinciples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.SaveAs = new System.Windows.Forms.Button();
+            this.NewFile = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("SimSun", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(794, 703);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save.Location = new System.Drawing.Point(812, 129);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(99, 30);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "保存";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // SaveAs
+            // 
+            this.SaveAs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveAs.Location = new System.Drawing.Point(812, 185);
+            this.SaveAs.Name = "SaveAs";
+            this.SaveAs.Size = new System.Drawing.Size(99, 30);
+            this.SaveAs.TabIndex = 2;
+            this.SaveAs.Text = "另存为";
+            this.SaveAs.UseVisualStyleBackColor = true;
+            this.SaveAs.Click += new System.EventHandler(this.SaveAs_Click);
+            // 
+            // NewFile
+            // 
+            this.NewFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NewFile.Location = new System.Drawing.Point(812, 77);
+            this.NewFile.Name = "NewFile";
+            this.NewFile.Size = new System.Drawing.Size(99, 30);
+            this.NewFile.TabIndex = 3;
+            this.NewFile.Text = "新建";
+            this.NewFile.UseVisualStyleBackColor = true;
+            this.NewFile.Click += new System.EventHandler(this.NewFile_Click);
+            // 
+            // Open
+            // 
+            this.Open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Open.Location = new System.Drawing.Point(812, 236);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(99, 30);
+            this.Open.TabIndex = 4;
+            this.Open.Text = "打开";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Run
+            // 
+            this.Run.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Run.Location = new System.Drawing.Point(812, 284);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(99, 30);
+            this.Run.TabIndex = 5;
+            this.Run.Text = "执行";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 727);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.NewFile);
+            this.Controls.Add(this.SaveAs);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ToyPL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button SaveAs;
+        private System.Windows.Forms.Button NewFile;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Run;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CompilationPrinciples
+{
+    public partial class Form1 : Form
+    {
+        private string currFilePath;
+        private OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        private SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+        private bool IsChanged = false;
+
+        //快捷键
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S && e.Control)
+            {
+                // 执行某个操作
+                this.Save_Click(sender, e);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            textBox1.Text = File.ReadAllText("code.txt");
+        }
+
+        private void NewFile_Click(object sender, EventArgs e)
+        {
+            if (IsChanged)
+            {
+                SaveDialogFun();
+            }
+            else
+            {
+                this.textBox1.Text = "";
+            }
+        }
+
+        private void SaveDialogFun()
+        {
+            DialogResult result = MessageBox.Show("是否保存更改？", "保存提示", MessageBoxButtons.YesNoCancel);
+
+            if (result == DialogResult.Yes)
+            {
+                saveFileDialog1.Filter = "PL files (*.pl)|*.pl|Text files (*.txt)|*.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveFileDialog1.FileName, this.textBox1.Text);
+                    this.currFilePath = saveFileDialog1.FileName;
+                }
+            }
+            else if (result == DialogResult.No)
+            {
+                this.textBox1.Text = "";
+            }
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.IsChanged = true;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(currFilePath))
+            {
+                if (IsChanged)
+                    File.WriteAllText(currFilePath, this.textBox1.Text);
+            }
+            else
+            {
+                saveFileDialog1.Filter = "PL(*,pl)|*.pl|文本(*,txt)|*.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveFileDialog1.FileName, this.textBox1.Text);
+                    this.currFilePath = saveFileDialog1.FileName;
+                }
+            }
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "PL(*,pl)|*.pl|文本(*,txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    this.textBox1.Text = reader.ReadToEnd();
+                }
+                this.currFilePath = path;
+                this.IsChanged = false;
+            }
+        }
+
+        private void SaveAs_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "PL files (*.pl)|*.pl|Text files (*.txt)|*.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, this.textBox1.Text);
+                this.currFilePath = saveFileDialog1.FileName;
+            }
+        }
+
+        private void begin_Parse()
+        {
+            if (this.textBox1.Text == null||this.textBox1.Text=="")
+            {
+                return;
+            }
+            else
+            {
+
+                string code = Regex.Replace(this.textBox1.Text, @"[\r]", "");
+
+                using (StreamWriter writer = new StreamWriter("output.txt", false, Encoding.GetEncoding("gb2312")))
+                {
+                    writer.Write(code);
+                }
+
+
+                Process p = Process.Start("toyPL.exe","output.txt");
+                p.WaitForExit();
+            }
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            begin_Parse();
+        }
+    }
+}
